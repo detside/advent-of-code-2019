093337a3bbbc24cef19847ec7dfadad922c741a4
Simon de Timary <simon.de.timary@bjss.com>
2019-12-04 11:28:00 +0000

    corrected a typo in the output

--- Day_4/index.js
+++ Day_4/index.js
@@ -1,6 +1,6 @@
 const { readFile, checkNumber } = require("./utils/utils");
 
-function testDay4Part1(inputFile) {
+function testDay4(inputFile) {
   let range = readFile(inputFile);
   let count = 0;
   for (let i = range[0]; i < range[1]; i++) {
@@ -13,5 +13,5 @@ function testDay4Part1(inputFile) {
 
 function testDay4Part2(inputFile) {}
 //console.log(checkNumber(111111));
-console.log("results day 4 part 1 = ", testDay4Part1("./inputs/inputs.txt"));
+console.log("results day 4 part 2 = ", testDay4("./inputs/inputs.txt"));
 //console.log("results day 3 part 2 = ", testDay3Part2("./inputs/inputs.txt"));
